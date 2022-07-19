f380f0f7befb42926c71a2997db054fd118ec299
Besmir Miftari <54418494+Besmirmiftari@users.noreply.github.com>
2022-07-19 17:55:16 +0200

    initial files

---
+++ BesmirMiftariPortfolio/BesmirMiftariPortfolio/API-s/CommentsController.cs
@@ -0,0 +1,30 @@
+﻿using BesmirMiftariPortfolio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BesmirMiftariPortfolio.API_s {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase {
+     
+        [HttpGet]
+        public IEnumerable<User> Get() {
+            return Comments.AllComments;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public OkResult Post(User newUserComment) {
+
+            Comments.AllComments.Add(newUserComment);
+
+            return Ok();
+        }
+       
+    }
+}
---
+++ BesmirMiftariPortfolio/BesmirMiftariPortfolio/Models/Comments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BesmirMiftariPortfolio.Models {
+    public static class Comments {
+
+
+        public static List<User> AllComments = new List<User>();
+
+        public static void defaultcomments() {
+
+            AllComments.Add(new User("User1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."));
+            AllComments.Add(new User("User2", "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."));
+            AllComments.Add(new User("User3", "When an unknown printer took a galley of type and scrambled it to make a type specimen book."));
+
+            
+
+        }
+
+    }
+}
